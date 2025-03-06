99ae484e35d8e9ca24f2fb0f797f51acc2195b73
Ephratha-merlin-S <ephratha20merlin@gmail.com>
2025-03-06 22:13:11 +0530

    Update 002.py

--- 002.py
+++ 002.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
 # Loading the dataset to a Pandas DataFrame
